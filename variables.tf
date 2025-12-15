49bb0d82d0e9b8fd93a21cd97cb83ed435efb0fc
jagadishg-ctl <125589575+jagadishg-ctl@users.noreply.github.com>
2025-12-15 21:22:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,57 @@
 
-variable "project_id" { type = string }variable
-variable "region"     { type = string }
-variable "zone"       { type = string }
+# Project & location
+variable "project_id" {
+  type = string
+}
 
-variable "impersonate_service_account" { type = string }
+variable "region" {
+  type = string
+}
 
-# VPC
-variable "network_name"   { type = string }
-variable "subnet_a_name"  { type = string }
-variable "subnet_a_cidr"  { type = string }
-variable "subnet_b_name"  { type = string }
-variable "subnet_b_cidr"  { type = string }
+variable "zone" {
+   type = string
+}
 
-# IAM
-variable "service_account_id"           { type = string }
-variable "service_account_display_name" { type = string }
+# Impersonation SA
+variable "impersonate_service_account" {
+  type = string
+}
+
+# VPC & Subnets
+variable "network_name" {
+  type = string
+}
+
+variable "subnet_a_name" {
+  type = string
+}
+
+variable "subnet_a_cidr" {
+  type = string
+}
+
+variable "subnet_b_name" {
+  type = string
+}
+
+variable "subnet_b_cidr" {
+  type = string
+}
+
+# IAM / Service Account
+variable "service_account_id" {
+  type = string
+}
+
+variable "service_account_display_name" {
+  type = string
+}
 
 # VM
-variable "vm_name"      { type = string }
+variable "vm_name" {
+  type = string
+}
+
+variable "machine_type" {
+  type = string
+
