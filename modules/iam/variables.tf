e43a84e440a5bf1d6a3553cbf121c840076a7c97
jagadishg-ctl <125589575+jagadishg-ctl@users.noreply.github.com>
2025-12-15 21:18:33 +0530

    Update variables.tf

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -1,3 +1,15 @@
 
-variable "project_id"                  { type = string }
-variablevariable "service_account_id"          { type = string }  # e.g., "galaxy"
+variable "project_id" {
+  type = string
+}
+
+variable "service_account_id" {
+  type        = string
+  description = "Service account ID (e.g., galaxy)"
+}
+
+variable "service_account_display_name" {
+  type        = string
+  description = "Display name for the service account"
+}
+
