f86da3fb784c043959f16d66a8c49d303040fa2b
jagadishg-ctl <125589575+jagadishg-ctl@users.noreply.github.com>
2025-12-15 20:55:37 +0530

    Update variables.tf

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -1,4 +1,8 @@
-variable "zone" {}
-variable "network_selflink" {}
-variable "subnet_selflink" {}
-variable "service_account" {}
+
+variable "project_id"            { type = string }
+variable "region"                { type = string }
+variable "zone"                  { type = string }
+variable "vm_name"               { type = string }
+variable "machine_type"          { type = string }
+variable "subnet_self_link"      { type = string }
+variable "service_account_email" { type = string }
