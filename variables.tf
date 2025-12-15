5f366c3b391e9a5265876d7ac193ba1c65fd7d7c
jagadishg-ctl <125589575+jagadishg-ctl@users.noreply.github.com>
2025-12-15 20:52:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,20 @@
-variable "region" {
-default = "us-central1"
-}
 
+variable "project_id" { type = string }variable
+variable "region"     { type = string }
+variable "zone"       { type = string }
 
-variable "zone" {
-default = "us-central1-a"
-}
+variable "impersonate_service_account" { type = string }
 
+# VPC
+variable "network_name"   { type = string }
+variable "subnet_a_name"  { type = string }
+variable "subnet_a_cidr"  { type = string }
+variable "subnet_b_name"  { type = string }
+variable "subnet_b_cidr"  { type = string }
 
-variable "network_name" {
-default = "shared-vpc"
-}
+# IAM
+variable "service_account_id"           { type = string }
+variable "service_account_display_name" { type = string }
+
+# VM
+variable "vm_name"      { type = string }
