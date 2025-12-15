09c5d25fd8d1e937ce245fa3215422e558a9bcf5
jagadishg-ctl <125589575+jagadishg-ctl@users.noreply.github.com>
2025-12-15 20:55:53 +0530

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,34 +1,38 @@
-resource "google_compute_instance" "web" {
-name = "web-vm"
-zone = var.zone
-machine_type = "e2-medium"
 
+resource "google_compute_instance" "vm" {
+  name         = var.vm_name
+  project      = var.project_id
+  zone         = var.zone
+  machine_type = var.machine_type
 
-boot_disk {
-initialize_params {
-image = "debian-cloud/debian-12"
-}
-}
+  boot_disk { initialize_params { image = "debian-cloud/debian-11" } }
 
+  network_interface {
+    subnetwork   = var.subnet    subnetwork   = var.subnet_self_link
+    access_config {}  # ephemeral external IP
+  }
 
-network_interface {
-network = var.network_selflink
-subnetwork = var.subnet_selflink
-access_config {}
-}
+  service_account {
+    email  = var.service_account_email
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
 
+  tags = ["http-server"]
 
-service_account {
-email = var.service_account
-scopes = ["cloud-platform"]
-}
-
-
-metadata_startup_script = <<-EOF
-#!/bin/bash
-apt-get update
-apt-get install -y apache2
-echo "<h1>Lumen Migration Factory</h1>" > /var/www/html/index.html
-systemctl restart apache2
-EOF
-}
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    apt-get update -y
+    apt-get install -y apache2
+    systemctl enable apache2
+    systemctl start apache2
+    cat > /var/www/html/index.html <<'HTML'
+    <!doctype html>
+    <html>
+      <head><title>Lumen Migration Factory</title></head>
+      <body style="font-family:sans-serif">
+        <h1>Hello from lumenmigrationfactory.com!</h1>
+        <p>Server: ${var.vm_name}</p>
+      </body>
+    </html>
+    HTML
+  EOT
