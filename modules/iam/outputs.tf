96eefee53b57ca8eb0cceb60410d5d3ff634f101
jagadishg-ctl <125589575+jagadishg-ctl@users.noreply.github.com>
2025-12-15 22:00:07 +0530

    Update outputs.tf

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,5 +1,7 @@
 
 output "service_account_email" {
-  value = data.google_service_account.vm_sa.email
+  value       = data.google_service_account.vm_sa.email
+  description = "Email of the existing VM service account"
 }
+
 
