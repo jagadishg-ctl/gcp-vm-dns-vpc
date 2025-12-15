687f111fc243e75141a3c114df33251ca57977ae
jagadishg-ctl <125589575+jagadishg-ctl@users.noreply.github.com>
2025-12-15 22:00:41 +0530

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,4 +1,9 @@
 
+data "google_service_account" "vm_sa" {
+  project    = var.project_id
+  account_id = var.service_account_id  # "galaxy"
+}
+
 # (Optional) Keep role bindings only if you want Terraform to ensure these roles exist.
 # If roles are already present or you prefer not to manage them via TF, you can delete these blocks.
 
@@ -13,5 +18,6 @@ resource "google_project_iam_member" "metric_writer" {
   role    = "roles/monitoring.metricWriter"
   member  = "serviceAccount:${data.google_service_account.vm_sa.email}"
 }
+
 
 
