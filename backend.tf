ebc46aed1fd4e3768497a80e7204b64c5a90673a
jagadishg-ctl <125589575+jagadishg-ctl@users.noreply.github.com>
2025-12-15 20:51:48 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
+
 terraform {
-backend "gcs" {
-bucket = "migration_gcs"
-prefix = "terraform/state/shared-vpc"
-impersonate_service_account = "galaxy@terraform-project-475116.iam.gserviceaccount.com"
-}
+  backend "gcs" {
+    bucket = "migration_gcs"
+    prefix = "state/stack-vm"
+  }
 }
