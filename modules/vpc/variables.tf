523e85cfdf727e66fa58e8d2ee78122f7d38b3c9
jagadishg-ctl <125589575+jagadishg-ctl@users.noreply.github.com>
2025-12-15 20:53:28 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,1 +1,8 @@
-variable "network_name" {}
+
+variable "project_id"   { type = string }
+variable "region"       { type = string }
+variable "network_name" { type = string }
+variable "subnet_a_name"{ type = string }
+variable "subnet_a_cidr"{ type = string }
+variable "subnet_b_name"{ type = string }
+variable "subnet_b_cidr"{ type = string }
