e56d2d64441b2e75aa7cd1d35853f8239f27c4c9
jagadishg-ctl <125589575+jagadishg-ctl@users.noreply.github.com>
2025-12-15 20:54:14 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,8 +1,15 @@
-output "network_selflink" {
-value = google_compute_network.shared_vpc.self_link
+
+output "network_name" {
+  value       = google_compute_network.shared_vpc.name
+  description = "Name of the shared VPC network"
 }
 
+output "subnet_a_self_link" {
+  value       = google_compute_subnetwork.subnet_a.self_link
+  description = "Self link of subnet A"
+}
 
-output "subnet1_selflink" {
-value = google_compute_subnetwork.subnet1.self_link
+output "subnet_b_self_link" {
+  value       = google_compute_subnetwork.subnet_b.self_link
+  description = "Self link of subnet B"
 }
