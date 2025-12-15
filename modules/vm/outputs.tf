102f2825eeb88a612ec39d5118bc7413eab4654b
jagadishg-ctl <125589575+jagadishg-ctl@users.noreply.github.com>
2025-12-15 20:56:13 +0530

    Update outputs.tf

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -1,3 +1,5 @@
-output "public_ip" {
-value = google_compute_instance.web.network_interface[0].access_config[0].nat_ip
+
+output "external_ip" {
+  value       = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+  description = "External IP of the VM"
 }
