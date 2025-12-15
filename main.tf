40dced4a1f1669a09abdf477a9f166c7894df2d3
jagadishg-ctl <125589575+jagadishg-ctl@users.noreply.github.com>
2025-12-15 20:52:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,38 @@
-module "vpc" {
-source = "./modules/vpc"
-network_name = var.network_name
+
+resource "google_project_service" "compute" {
+  project            = var.project_id
+  service            = "compute.googleapis.com"
+  disable_on_destroy = false
 }
 
+module "vpc" {
+  source         = "./modules/vpc"
+  project_id     = var.project_id
+  region         = var.region
+  network_name   = var.network  network_name   = var.network_name
+  subnet_a_name  = var.subnet_a_name
+  subnet_a_cidr  = var.subnet_a_cidr
+  subnet_b_name  = var.subnet_b_name
+  subnet_b_cidr  = var.subnet_b_cidr
+
+  depends_on = [google_project_service.compute]
+}
 
 module "iam" {
-source = "./modules/iam"
+  source                       = "./modules/iam"
+  project_id                   = var.project_id
+  service_account_id           = var.service_account_id
+  service_account_display_name = var.service_account_display_name
 }
-
 
 module "vm" {
-source = "./modules/vm"
-zone = var.zone
-network_selflink = module.vpc.network_selflink
-subnet_selflink = module.vpc.subnet1_selflink
-service_account = module.iam.service_account_email
+  source                = "./modules/vm"
+  project_id            = var.project_id
+  region                = var.region
+  zone                  = var.zone
+  vm_name               = var.vm_name
+  machine_type          = var.machine_type
+  subnet_self_link      = module.vpc.subnet_a_self_link
+  service_account_email = module.iam.service_account_email
 
-
-depends_on = [module.vpc, module.iam]
-}
-
-
-module "dns" {
-source = "./modules/dns"
-vm_public_ip = module.vm.public_ip
-
-
-depends_on = [module.dns, module.vm]
-}
+  depends_on = [module.vpc, google_project_service.compute]
