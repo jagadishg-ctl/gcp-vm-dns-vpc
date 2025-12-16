73c5ece98df8bd560a4999475e9eb3dd7f0f3c30
jagadishg-ctl <125589575+jagadishg-ctl@users.noreply.github.com>
2025-12-16 17:26:01 +0530

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -4,7 +4,7 @@ resource "google_compute_instance" "vm" {
   project      = var.project_id
   zone         = var.zone
   machine_type = var.machine_type
-
+}
   boot_disk {
     initialize_params { image = "debian-cloud/debian-11" }
   }
@@ -38,4 +38,5 @@ resource "google_compute_instance" "vm" {
     </html>
     HTML
   EOT
+
 
